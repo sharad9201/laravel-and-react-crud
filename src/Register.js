f5f6f175e410324f8a61fe30173bc21e4da7f955
sharad9201 <sharad.rae19@gmail.com>
2021-07-18 20:42:47 +0545

    api fetch for signup

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,44 @@
+import React ,{useState} from 'react'
+import {useHistory} from 'react-router-dom'
+
 function Register() {
+
+    const [name,setName]=useState('')
+    const [password,setPassword]=useState('')
+    const [email,setEmail]=useState('')
+    const history= useHistory()
+
+    async function signUp(){
+        let item={name,password,email}
+
+        let result = await fetch('http://localhost:8000/api/register',{
+            method:'POST',
+            body:JSON.stringify(item),
+            headers:{
+                "Content-Type":'application/json',
+                "Accept" : 'application/json'
+
+            }
+        })
+
+        result = await result.json()
+
+        localStorage.setItem('user-info',JSON.stringify(result))
+        history.push('/add')
+        console.warn("result", result)
+        // console.warn(name,email,password)
+    }
+
     return(
-        <h1> register page</h1>
+        <div className="col-sm-6 offset-sm-3">
+            <h1> register page</h1>
+            {/* <label htmlFor="name">Name</label> */}
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)}  className="form-control" placeholder="name" /><br />
+            <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}  className="form-control" placeholder="email" /><br />
+            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}  className="form-control" placeholder="password" /><br />
+            <button onClick={signUp} className="btn btn-primary"> Sign Up</button>
+        </div>
+        
     )
 }
 
