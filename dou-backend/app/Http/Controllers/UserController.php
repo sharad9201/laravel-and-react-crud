457d0f0c10052283d1b764166335bd753eaefab3
sharad9201 <sharad.rae19@gmail.com>
2021-07-18 20:35:05 +0545

    api integration in sign up

--- dou-backend/app/Http/Controllers/UserController.php
+++ dou-backend/app/Http/Controllers/UserController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     //
     function register(Request $req){
 
-        return $req->input();
+        // $user= new user;
+        // $user->name= $req->input('name');
+        // $user->email= $req->input('email');
+        // $user->password= Hash::make($req->input('password'));
+
+        // $user->save();
+        $user= User::create($req->all());
+
+        return $user;
     }
 }
